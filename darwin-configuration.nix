268515fdf9b330b68f4ca9e5a18df49de501506f
Smaug123 <patrick+github@patrickstevens.co.uk>
2021-07-04 22:45:24 +0100

    Attempt to use home-manager module

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -167,18 +167,20 @@ let
 
 in
 {
+
+  imports = [ <home-manager/nix-darwin> ];
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users.Patrick = import ./home.nix;
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
 
   environment.systemPackages =
-    [ pkgs.neovim
-      pkgs.alacritty
+    [ pkgs.alacritty
       pkgs.tmux
       pkgs.wget
       pkgs.youtube-dl
-      pkgs.git
-      pkgs.fzf
-      pkgs.shellcheck
       pkgs.cmake
       pkgs.gcc
       pkgs.gdb
@@ -186,9 +188,7 @@ in
       pkgs.hledger-web
       pkgs.dotnet-sdk_5
       pkgs.docker
-      pkgs.ycmd
       pkgs.keepassxc
-      pkgs.oh-my-zsh
       pkgs.jitsi-meet
       pkgs.elan
       pkgs.protonmail-bridge
@@ -209,10 +209,6 @@ in
   # Auto upgrade nix package and the daemon service.
   # services.nix-daemon.enable = true;
   # nix.package = pkgs.nix;
-
-  # Create /etc/bashrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
-  # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
