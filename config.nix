bcf7acd881a4d662d4c6201366fbe88f17d4ceb5
Smaug123 <patrick+github@patrickstevens.co.uk>
2021-10-16 09:50:50 +0100

    Remove spare line

--- config.nix
+++ config.nix
@@ -1,7 +1,6 @@
 {
   packageOverrides = pkgs: {
     gmp = pkgs.gmp.override { postInstall = ''
-      echo hello
       ln -s /usr/local/opt/gmp/lib/libgmp.10.dylib $out/lib/libgmp.10.dylib
       '';
     };
