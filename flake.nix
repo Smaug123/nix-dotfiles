e2ee831e5f33fe0edc79bec700cec7dc57b03270
Smaug123 <patrick+github@patrickstevens.co.uk>
2022-02-06 13:36:17 +0000

    Add emacs overlay

--- flake.nix
+++ flake.nix
@@ -11,16 +11,21 @@
       url = "github:lnl7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    emacs = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, darwin, nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, darwin, emacs, nixpkgs, home-manager, ... }@inputs:
     let system = "aarch64-darwin"; in
     let config = {
       allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
         "vscode"
       ];
     }; in
-    let pkgs = (import nixpkgs { inherit system config; }); in
+    let overlays = [ emacs.overlay ] ++ import ./overlays.nix; in
+    let pkgs = (import nixpkgs { inherit system config overlays; }); in
     {
       darwinConfigurations = {
         nixpkgs = pkgs;
