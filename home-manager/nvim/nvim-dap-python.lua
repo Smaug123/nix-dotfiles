f723b644865d0e5179f2cbe76ab21e1e15a46591
Smaug123 <patrick+github@patrickstevens.co.uk>
2024-04-03 21:21:46 +0100

    Add more debugger-related commands in Python

--- home-manager/nvim/nvim-dap-python.lua
+++ home-manager/nvim/nvim-dap-python.lua
@@ -1,1 +1,17 @@
 require("dap-python").setup("%PYTHONENV%/bin/python")
+
+do
+	local whichkey = require("which-key")
+	whichkey.register({
+		p = {
+			d = {
+				"Debugger-related commands",
+				t = {
+					"Tests",
+					f = { require("dap-python").test_class, "Run Python tests in the current file" },
+					c = { require("dap-python").test_method, "Run the Python test under the cursor" },
+				},
+			},
+		},
+	}, { prefix = vim.api.nvim_get_var("maplocalleader") })
+end
