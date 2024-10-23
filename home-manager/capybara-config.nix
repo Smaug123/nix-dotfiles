df071387b28e490305f75cb6062bf3ca4c92b841
Patrick Stevens <3138005+Smaug123@users.noreply.github.com>
2024-10-23 22:00:06 +0100

    Pipewire on Capybara (#88)

--- home-manager/capybara-config.nix
+++ home-manager/capybara-config.nix
@@ -14,6 +14,14 @@
     enable = true;
   };
 
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
