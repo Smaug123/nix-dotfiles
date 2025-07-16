ff162df10adc924dd0ed4bea0de74a2441a1412e
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2025-07-16 20:44:36 +0100

    Tmux copy-paste

--- home-manager/modules/tmux.nix
+++ home-manager/modules/tmux.nix
@@ -14,6 +14,18 @@
     extraConfig = ''
       set-option -sa terminal-features ',xterm-256color:RGB'
       set -g default-command "exec ${pkgs.zsh}/bin/zsh"
+      
+      # Vi mode
+      set-window-option -g mode-keys vi
+      
+      # Use v to begin selection in copy mode
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      
+      # Use Shift+V to select line
+      bind-key -T copy-mode-vi V send-keys -X select-line
+      
+      # Use y to yank to clipboard
+      bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "${pkgs.wl-clipboard}/bin/wl-copy"
     '';
   };
 }
