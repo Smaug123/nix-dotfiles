09c6a60f1afa2365806d86e219e12e1a52ac4532
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2026-04-26 10:47:13 +0100

    Format

--- home-manager/modules/direnv.nix
+++ home-manager/modules/direnv.nix
@@ -2,9 +2,8 @@
   # FIXME(nixpkgs#513019): direnv's checkPhase can hang on aarch64-darwin
   # while fish/zsh substitutes are served with broken code signatures.
   # Remove this once nixpkgs#513081 or an equivalent upstream fix lands.
-  direnv =
-    assert pkgs.lib.assertMsg (pkgs.direnv.version == "2.37.1" && (pkgs.direnv.doCheck or true))
-      "direnv workaround may no longer be needed: direnv=${pkgs.direnv.version}, doCheck=${pkgs.lib.boolToString (pkgs.direnv.doCheck or true)}. Try removing it.";
+  direnv = assert pkgs.lib.assertMsg (pkgs.direnv.version == "2.37.1" && (pkgs.direnv.doCheck or true))
+  "direnv workaround may no longer be needed: direnv=${pkgs.direnv.version}, doCheck=${pkgs.lib.boolToString (pkgs.direnv.doCheck or true)}. Try removing it.";
     pkgs.direnv.overrideAttrs (_: {
       doCheck = false;
     });
