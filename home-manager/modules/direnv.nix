dbcd543ab14c7c8f90201908b6dbd812e4db1a17
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2026-04-26 10:44:53 +0100

    Disable direnv checks

--- home-manager/modules/direnv.nix
+++ home-manager/modules/direnv.nix
@@ -1,10 +1,21 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  # FIXME(nixpkgs#513019): direnv's checkPhase can hang on aarch64-darwin
+  # while fish/zsh substitutes are served with broken code signatures.
+  # Remove this once nixpkgs#513081 or an equivalent upstream fix lands.
+  direnv =
+    assert pkgs.lib.assertMsg (pkgs.direnv.version == "2.37.1" && (pkgs.direnv.doCheck or true))
+      "direnv workaround may no longer be needed: direnv=${pkgs.direnv.version}, doCheck=${pkgs.lib.boolToString (pkgs.direnv.doCheck or true)}. Try removing it.";
+    pkgs.direnv.overrideAttrs (_: {
+      doCheck = false;
+    });
+in {
   home.packages = [
-    pkgs.direnv
+    direnv
   ];
   programs.direnv = {
     enable = true;
     enableZshIntegration = true;
+    package = direnv;
     nix-direnv.enable = true;
     # stdlib = builtins.readFile ../direnv/envrc;
   };
